f185ba984d30c1cb1af46dd403baf33cb01c9eea
Daniel <djarocki7@gmail.com>
2023-03-25 16:00:59 +0100

    updater corrections

--- iCuPlus-Files/WindowsFormsApp1/WindowsFormsApp1/Form3.cs
+++ iCuPlus-Files/WindowsFormsApp1/WindowsFormsApp1/Form3.cs
@@ -45,7 +45,7 @@ namespace WindowsFormsApp1
 
                     {
                         WebClient webClient = new WebClient();
-                        webClient.DownloadFile("https://github.com/DsSoft-Byte/iCu-X/releases/download/v10/do.not.download.zip", @"C:\iCuPlus.zip");
+                        webClient.DownloadFile("https://raw.githubusercontent.com/DsSoft-Byte/iCu-X/main/iCures.zip", @"C:\iCuPlus.zip");
                         string zipPath = @"C:\iCuPlus.zip";
                         string extractPath = @"C:\UpdateData";
                         System.IO.Compression.ZipFile.ExtractToDirectory(zipPath, extractPath);
